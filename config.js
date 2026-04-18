61be3a8515b582c035ed6c8e934a040da76f4bb5
kikimyomyo <request1004@gmail.com>
2026-04-18 12:54:01 +0900

    deploy: parts-of-speech-v2

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "parts-of-speech-v2",
+  title: "품사와 문장성분 게임 v2",
+  sheet_tab: "parts-of-speech-v2",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
